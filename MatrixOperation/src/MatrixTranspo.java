d34edd241f1f5641d36f0a9ea231e43e2a2c37e1
COSTE Julie <julieanne.coste@gmail.com>
2024-03-30 15:31:45 +0100

    thread ok

--- MatrixOperation/src/MatrixTranspo.java
+++ MatrixOperation/src/MatrixTranspo.java
@@ -23,10 +23,26 @@ public class MatrixTranspo extends Matrix {
         System.out.println(segment);
         System.out.println(numThreads);
 
-        for (int i = 0; i < matrix.getLines(); i++) {
-            for (int j = 0; j < matrix.getColumn(); j++) {
-                this.setValues(j, i, matrix.getValues(i, j));
+        Thread[] thread = new Thread[matrix.getColumn() / segment];
+        for (int i = 0; i < matrix.getColumn() / segment; i++) {
+            final int row = i;
+            Runnable task = () -> transposeRow(row, matrix);
+            thread[i] = new Thread(task);
+            thread[i].start();
+        }
+
+        for (Thread threads : thread) {
+            try {
+                threads.join();
+            } catch (InterruptedException e) {
+                System.out.println("erreur");
             }
+        }
+    }
+
+    public void transposeRow(int row, Matrix matrix) {
+        for (int i = 0; i < matrix.getColumn(); i++) {
+            this.setValues(i, row, matrix.getValues(row, i));
         }
     }
 
