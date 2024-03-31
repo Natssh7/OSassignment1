da75a5023397520941bc1a69543adb7b309d0fc1
Yunck02 <soupramanienyannick@gmail.com>
2024-03-31 16:02:18 -0400

    matrixMultiplication
    multiplication de 2 matrices a et b

--- MatrixOperation/src/MatrixAdd.java
+++ MatrixOperation/src/MatrixAdd.java
@@ -5,6 +5,7 @@ import java.util.InputMismatchException;
 
 public class MatrixAdd {
     private int rows;
+    abc
     private int cols;
     private int[][] data;
 
--- MatrixOperation/src/MatrixMultiplication.java
+++ MatrixOperation/src/MatrixMultiplication.java
@@ -1,3 +1,24 @@
-public class MatrixMultiplication {
+public class MatrixMultiplication extends Matrix{
 
+    public MatrixMultiplication(Matrix matrixA, Matrix matrixB){
+        super(matrixA.getLines(),matrixB.getColumn(),0);
+    }
+    public static Matrix Multiplication(Matrix matrixA, Matrix matrixB){
+        MatrixMultiplication matrixC = new MatrixMultiplication(matrixA,matrixB);
+        if(matrixA.getColumn() == matrixB.getLines()){
+            for (int i = 0; i < matrixC.getLines(); i++) {
+                for (int j = 0; j < matrixC.getColumn(); j++) {
+                    int c = 0;
+                    for (int k = 0; k < matrixA.getColumn(); k++){
+                        c += matrixA.getValues(i, k)*matrixB.getValues(k, j);
+                    }
+                    matrixC.setValues(i, j, c);
+                }
+            }
+        }
+        else{
+            System.out.println("Error: impossible operation asked");
+        }
+        return matrixC;
+    }
 }
